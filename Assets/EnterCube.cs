92006cc78634762831fbb3b70093bdd5c21bd183
Adam Biggs <wareaglebiggs@gmail.com>
2023-11-20 23:25:54 -0600

    Attempting referee system

--- Assets/EnterCube.cs
+++ Assets/EnterCube.cs
@@ -22,28 +22,42 @@ public class EnterCube : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "O" || other.tag == "X")
+        if ((other.CompareTag("O") || other.CompareTag("X")) && !isTaken && !HGI.IsGrabbing)
         {
-            if(!isTaken){
-            mRend.enabled = true;
-            }
+            // Snap the piece in place
+            isTaken = true;
+            HandGrabInteractable grab = other.gameObject.GetComponent<HandGrabInteractable>();
+            grab.enabled = false;
+            other.gameObject.transform.position = this.transform.position;
+            mRend.enabled = false;
 
-            
-            if (!HGI.IsGrabbing && isTaken == false)
+            // Parse the cube's name to get its layer, column, and row
+            string[] splitName = this.gameObject.name.Split('_');
+            if (splitName.Length == 4 && splitName[0] == "Cube")
             {
-                //snap
-                isTaken = true;
-                
-                
-                
-                HandGrabInteractable grab = other.gameObject.GetComponent<HandGrabInteractable>();
-                grab.enabled = false;
+                // Subtract 1 to convert to 0-based index
+                int layer = int.Parse(splitName[1]) - 1;
+                int column = int.Parse(splitName[2]);
+                int row = int.Parse(splitName[3]);
 
-                other.gameObject.transform.position = this.transform.position;
-                
-                mRend.enabled = false;
-                
-                gm.TakeTurn();
+                // Check if layer is within bounds (since it's 0-indexed after subtraction)
+                if(layer >= 0 && layer < 4)
+                {
+                    // Update the board and check for a win
+                    bool won = gm.TakeTurn(layer, column, row);
+                    if (won)
+                    {
+                        Debug.Log(gm.turn == 0 ? "X Wins!" : "O Wins!");
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Cube layer index out of bounds: " + this.gameObject.name);
+                }
+            }
+            else
+            {
+                Debug.LogError("Invalid cube name for determining grid position: " + this.gameObject.name);
             }
         }
     }
--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -29,44 +29,170 @@ public class GameMaster : MonoBehaviour
     private GameObject instantiatedObj;
     public GameObject parent_X;
     public GameObject parent_O;
+    
+    private const int Empty = 0;
+    private const int PlayerX = 1; // Blue is X
+    private const int PlayerO = 2; // Orange is O
+    // The 4x4x4 board, initialized to some value indicating an empty cell
+    private int[,,] board = new int[4, 4, 4];
 
+    // Call this method after each move to check for a win
+private bool CheckForWin(int lastX, int lastY, int lastZ)
+{
+    int player = board[lastX, lastY, lastZ];
+
+    // Check along x, y, z axes
+    for (int i = 0; i < 4; i++)
+    {
+        if (board[i, lastY, lastZ] != player || 
+            board[lastX, i, lastZ] != player || 
+            board[lastX, lastY, i] != player)
+        {
+            return false;
+        }
+    }
+
+    // Check diagonal on the xy plane
+    if (lastX == lastY)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (board[i, i, lastZ] != player)
+            {
+                return false;
+            }
+        }
+    }
+
+    // Check anti-diagonal on the xy plane
+    if (lastX + lastY == 3)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (board[i, 3 - i, lastZ] != player)
+            {
+                return false;
+            }
+        }
+    }
+
+    // Check diagonal on the xz plane
+    if (lastX == lastZ)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (board[i, lastY, i] != player)
+            {
+                return false;
+            }
+        }
+    }
+
+    // Check anti-diagonal on the xz plane
+    if (lastX + lastZ == 3)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (board[i, lastY, 3 - i] != player)
+            {
+                return false;
+            }
+        }
+    }
+
+    // Check diagonal on the yz plane
+    if (lastY == lastZ)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (board[lastX, i, i] != player)
+            {
+                return false;
+            }
+        }
+    }
+
+    // Check anti-diagonal on the yz plane
+    if (lastY + lastZ == 3)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (board[lastX, i, 3 - i] != player)
+            {
+                return false;
+            }
+        }
+    }
+
+    // Check 3D diagonals
+    if (lastX == lastY && lastY == lastZ)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (board[i, i, i] != player)
+            {
+                return false;
+            }
+        }
+    }
+
+    if (lastX == lastY && lastX + lastZ == 3)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (board[i, i, 3 - i] != player)
+            {
+                return false;
+            }
+        }
+    }
+
+    // If none of the above checks returned false, there's a win
+    return true;
+}
     private void Start()
     {
         
     }
 
     // Call this method when a turn is taken
-    public void TakeTurn()
+    public bool TakeTurn(int layer, int column, int row)
     {
-        // Switch turn
-        turn = 1 - turn; // This will toggle between 0 and 1
+        // Correct the indices to match the 0-indexed array.
+        int x = layer - 1;
+        int y = column - 1;
+        int z = row - 1;
+
+        // First, check if the indices are within the bounds of the board array
+        if (x < 0 || x >= 4 || y < 0 || y >= 4 || z < 0 || z >= 4)
+        {
+            Debug.LogError($"Attempted to TakeTurn with invalid indices: layer={layer}, column={column}, row={row}");
+            return false; // Indices out of bounds, exit the method
+        }
+
+        // Update the board with the current player's move
+        board[x, y, z] = turn == 0 ? PlayerX : PlayerO;
+
+        // Check for a win after the move
+        bool hasWon = CheckForWin(x, y, z);
+
+        // Switch turn for the next player
+        turn = 1 - turn;
 
         // Instantiate the object based on the turn and set its position
-        if (turn == 0)
-        {
-            instantiatedObj = Instantiate(obj_x, obj_x.transform.position, Quaternion.Euler(90, 45, 0)); // Add the position and default rotation
-            instantiatedObj.transform.SetParent(parent_X.transform, false); // false to not apply world position to local
-            instantiatedObj.transform.localScale = obj_x.transform.localScale; // Set the local scale to match obj_x
-            instantiatedObj.transform.localPosition = Vector3.zero; // Set local position to (0,0,0)
-            
-            
-        }
-        else if (turn == 1)
-        {
-            instantiatedObj = Instantiate(obj_o, obj_o.transform.position, Quaternion.identity); // Add the position and default rotation
-            instantiatedObj.transform.SetParent(parent_O.transform, false); // false to not apply world position to local
-            instantiatedObj.transform.localScale = obj_o.transform.localScale; // Set the local scale to match obj_x
-            instantiatedObj.transform.localPosition = Vector3.zero; // Set local position to (0,0,0)
-            
-            
-        }
+        instantiatedObj = Instantiate(turn == 0 ? obj_x : obj_o);
+        instantiatedObj.transform.SetParent(turn == 0 ? parent_X.transform : parent_O.transform, false);
+        instantiatedObj.transform.localScale = turn == 0 ? obj_x.transform.localScale : obj_o.transform.localScale;
+        instantiatedObj.transform.localPosition = new Vector3(x, y, z); // Use the corrected 0-based indices here
 
         // Activate the instantiated object
-        Debug.Log("inst");
         instantiatedObj.SetActive(true);
+        Debug.Log("inst");
 
-        
+        // Return whether the current player has won
+        return hasWon;
     }
+
 
     void Update()
     {
