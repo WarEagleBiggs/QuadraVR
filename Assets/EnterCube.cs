887eceb96c2bbe9dd2ee178b4811c7f80b920ce3
Adam Biggs <wareaglebiggs@gmail.com>
2023-11-20 23:51:54 -0600

    Revert "Attempting referee system"
    This reverts commit 92006cc78634762831fbb3b70093bdd5c21bd183.

--- Assets/EnterCube.cs
+++ Assets/EnterCube.cs
@@ -22,42 +22,28 @@ public class EnterCube : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if ((other.CompareTag("O") || other.CompareTag("X")) && !isTaken && !HGI.IsGrabbing)
+        if (other.tag == "O" || other.tag == "X")
         {
-            // Snap the piece in place
-            isTaken = true;
-            HandGrabInteractable grab = other.gameObject.GetComponent<HandGrabInteractable>();
-            grab.enabled = false;
-            other.gameObject.transform.position = this.transform.position;
-            mRend.enabled = false;
-
-            // Parse the cube's name to get its layer, column, and row
-            string[] splitName = this.gameObject.name.Split('_');
-            if (splitName.Length == 4 && splitName[0] == "Cube")
-            {
-                // Subtract 1 to convert to 0-based index
-                int layer = int.Parse(splitName[1]) - 1;
-                int column = int.Parse(splitName[2]);
-                int row = int.Parse(splitName[3]);
-
-                // Check if layer is within bounds (since it's 0-indexed after subtraction)
-                if(layer >= 0 && layer < 4)
-                {
-                    // Update the board and check for a win
-                    bool won = gm.TakeTurn(layer, column, row);
-                    if (won)
-                    {
-                        Debug.Log(gm.turn == 0 ? "X Wins!" : "O Wins!");
-                    }
-                }
-                else
-                {
-                    Debug.LogError("Cube layer index out of bounds: " + this.gameObject.name);
-                }
+            if(!isTaken){
+            mRend.enabled = true;
             }
-            else
+
+            
+            if (!HGI.IsGrabbing && isTaken == false)
             {
-                Debug.LogError("Invalid cube name for determining grid position: " + this.gameObject.name);
+                //snap
+                isTaken = true;
+                
+                
+                
+                HandGrabInteractable grab = other.gameObject.GetComponent<HandGrabInteractable>();
+                grab.enabled = false;
+
+                other.gameObject.transform.position = this.transform.position;
+                
+                mRend.enabled = false;
+                
+                gm.TakeTurn();
             }
         }
     }
