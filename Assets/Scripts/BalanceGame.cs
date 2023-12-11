053a4d6a5d8fb4e2d9773ae5ada6d650ef6b0e2b
Adam Biggs <wareaglebiggs@gmail.com>
2023-12-11 15:38:23 -0600

    Arrow Model

--- Assets/Scripts/BalanceGame.cs
+++ Assets/Scripts/BalanceGame.cs
@@ -6,11 +6,12 @@ public class BalanceGame : MonoBehaviour
 {
     public GameObject gameBoard;
     public Quaternion quats;
-    
+
 
     // Update is called once per frame
     void Update()
     {
+        //gameBoard.transform.eulerAngles = new Vector3(-90, gameBoard.transform.eulerAngles.y, 0);
         gameBoard.transform.rotation = quats;
     }
 }
