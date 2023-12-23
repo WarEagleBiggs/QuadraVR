a25cc811014b4e08a871ff432eca4fed095242ff
Adam Biggs <wareaglebiggs@gmail.com>
2023-12-22 22:32:58 -0600

    adds GameMatrix data structure that will help when developing the AI

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -5,11 +5,31 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 
+public enum PlayerType
+{
+    X = 0,
+    O = 1
+}
+
+public class GameCellEntry
+{
+    public bool m_IsOccupied;
+    public GameObject m_PlayerObj;
+    public PlayerType m_PlayerType;
+
+    public GameCellEntry()
+    {
+        m_IsOccupied = false;
+        m_PlayerObj = null;
+        m_PlayerType = PlayerType.X;
+    }
+}
+
 public class GameMaster : MonoBehaviour
 {
     //blue is x
     //orange is o
-    
+
     //turn == 0 is blue turn
     //turn == 1 is orange turn
     public int turn = 1;
@@ -40,9 +60,66 @@ public class GameMaster : MonoBehaviour
     public bool isAiGame = false;
 
     public bool isMainMenu;
-    
-    private void Start()
+
+    public List<Transform> m_GridLayers = new List<Transform>();
+
+    private GameCellEntry[,,] m_GameMatrix = new GameCellEntry[4, 4, 4];
+
+    private void PopulateGameMatrix()
     {
+        if (m_GridLayers.Count != 4)
+        {
+            Debug.LogError("Error, grid layers should be 4");
+        }
+        else
+        {
+            Vector3Int coord = Vector3Int.zero;
+            // --- bottom to top (y) 16 children ---
+            foreach (var layer in m_GridLayers)
+            {
+                coord.z = 0;
+                // --- left to right (x) ---
+                foreach (Transform cell in layer)
+                {
+                    EnterCube entry = cell.GetComponent<EnterCube>();
+                    // store coordinate
+                    entry.m_GridCoord = coord;
+                    coord.z = coord.x == 3 ? coord.z + 1 : coord.z;
+                    coord.x = coord.x < 3 ? coord.x + 1 : 0;
+                }
+
+                coord.y = coord.y < 3 ? coord.y + 1 : 0;
+            }
+        }
+
+        // --- initialize GameObjects with GameCellEntry ---
+        for (int x = 0; x < 4; ++x) {
+            for (int y = 0; y < 4; ++y) {
+                for (int z = 0; z < 4; ++z) {
+                    m_GameMatrix[x, y, z] = new GameCellEntry();
+                }
+            }
+        }
+
+    }
+
+    public GameCellEntry GetGridCell(Vector3Int coord)
+    {
+        GameCellEntry ret = null;
+        if (m_GameMatrix != null)
+        {
+            ret = m_GameMatrix[coord.x, coord.y, coord.z];
+        }
+
+        return ret;
+    }
+
+
+private void Start()
+    {
+        // populate the layout of the game matrix
+        PopulateGameMatrix();
+        
         if (SceneManager.GetActiveScene().name == "Menu")
         {
             isMainMenu = true;
@@ -82,8 +159,7 @@ public class GameMaster : MonoBehaviour
             instantiatedObj.transform.SetParent(parent_X.transform, false); // false to not apply world position to local
             instantiatedObj.transform.localScale = obj_x.transform.localScale; // Set the local scale to match obj_x
             instantiatedObj.transform.localPosition = Vector3.zero; // Set local position to (0,0,0)
-            
-            
+
         }
         else if (turn == 1 && isGamePlaying)
         {
